ca8e9bbeccd9ff4eb55780605eacfa28fc3633e2
SDuSDi <bmartinez.ext@catec.aero>
2024-04-30 09:54:11 +0200

    Push 30/04/24
    Added service client and listener to topic to communicate with said client
    Added functions for new client and listener

--- workspace/src/core/src/offb_node.cpp
+++ workspace/src/core/src/offb_node.cpp
@@ -9,35 +9,39 @@
 #include "px4_msgs/msg/vehicle_global_position.hpp"
 
 std::array<float,3> global_pos;
+uint16_t command_code;
 
 // Subnormal
 
 class ControlNode : public rclcpp::Node
 {
 public:
-    ControlNode() : Node("control_node")
+    ControlNode() : Node("ddscore")
     {
         // Definition of quality of service for all subscribers
         rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
         auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 
-        takeoff_cl = node -> create_client<px4_msgs::srv::VehicleCommand>("/fmu/vehicle_command");
+        // Definition of service clients
+        command_cl = this -> create_client<px4_msgs::srv::VehicleCommand>("/fmu/vehicle_command");
 
-        // Definition of real subscribers
-        global_position_check = node -> create_subscription<px4_msgs::msg::VehicleGlobalPosition>("/fmu/out/vehicle_global_position",qos,
-        std::bind());
-        takeoff_srv = node -> create_subscription<std_msgs::msg::Empty>("/ddscore/takeoff",qos, );
+        // Definition of subscribers
+        global_position_check = this -> create_subscription<px4_msgs::msg::VehicleGlobalPosition>("/fmu/out/vehicle_global_position",qos,
+                                std::bind(&ControlNode::global_pos_cb, this, std::placeholders::_1));
+        service_commander = this -> create_subscription<std_msgs::msg::Empty>("/ddscore/takeoff",qos, 
+                            std::bind(&ControlNode::publish_srv_command, this, std::placeholders::_1));
     }
 
 private:
 
     void global_pos_cb(px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg);
-    void publish_takeoff(const std_msgs::msg::Empty::SharedPtr msg);
+    void cb_srv_command(const rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future);
+    void publish_srv_command(const std_msgs::msg::Empty::SharedPtr msg);
 
-    rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr takeoff_cl;
+    rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr command_cl;
 
     rclcpp::Subscription<px4_msgs::msg::VehicleGlobalPosition>::SharedPtr global_position_check;
-    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr takeoff_srv;
+    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr service_commander;
 
 };
 
@@ -45,7 +49,17 @@ void ControlNode::global_pos_cb(const px4_msgs::msg::VehicleGlobalPosition::Shar
     global_pos[1] = msg -> lat; global_pos[2] = msg -> lon; global_pos[3] = msg -> alt;
 }
 
-void ControlNode::publish_takeoff(const std_msgs::msg::Empty::SharedPtr msg) {
+void ControlNode::cb_srv_command(const rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future) {
+    auto status = future.wait_for(std::chrono::seconds(1));
+    if (status == std::future_status::ready) {
+        RCLCPP_INFO(this -> get_logger(), 
+        std::to_string(future.get() -> reply.command));
+    } else {
+        RCLCPP_INFO(this -> get_logger(), "Service in progress");
+    }
+}
+
+void ControlNode::publish_srv_command(const std_msgs::msg::Empty::SharedPtr msg) {
     (void) msg; //Shuts the compiler up about the empty message not being used
     RCLCPP_INFO(this -> get_logger(), "Hey! I'm going to take off!");
 
@@ -55,7 +69,7 @@ void ControlNode::publish_takeoff(const std_msgs::msg::Empty::SharedPtr msg) {
 
     command.param5 = global_pos[1]; // Latitude
     command.param6 = global_pos[2]; // Longitude
-	command.param7 = global_pos[3]; // Absolute altitude + takeoff altitude
+	command.param7 = global_pos[3] + 2.50; // Absolute altitude + takeoff altitude
 
 	command.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_TAKEOFF;
 	command.target_system = 1;
@@ -66,12 +80,28 @@ void ControlNode::publish_takeoff(const std_msgs::msg::Empty::SharedPtr msg) {
     command.timestamp = this -> get_clock() -> now().nanoseconds() / 1000;
 
     request -> request = command;
-
+    while(!command_cl -> wait_for_service(std::chrono::seconds(1)))
+    {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(this -> get_logger(), "Interrupted while waiting for the service. Exiting...");
+            return;
+        }
+        RCLCPP_INFO(this -> get_logger(), "Service not available. Waiting again...");
+    }
+    auto result = command_cl -> async_send_request(request, std::bind(&ControlNode::cb_srv_command, this, std::placeholders::_1));
 }
+
+// void ControlNode::set_takeoff(const std_msgs::msg::Empty::SharedPtr msg) {
+//     (void) msg; //Shuts the compiler up about the empty message not being used
+
+// }
 
 int main(int argc, char **argv){
 
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ControlNode>());
     rclcpp::shutdown();
+
+    return 0;
+
 }
