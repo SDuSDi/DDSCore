fc7bb3cfc35ec7ac6fa72719ad0a5c648557ae7e
SDuSDi <bmartinez.ext@catec.aero>
2024-04-30 11:57:34 +0200

    Push 30/04/24
    Correct print of ack received from vehicle command
    Separated land and takeoff from srv sending
    Made takeoff take altitude of takeoff as only argument

--- workspace/build/core/CMakeFiles/node.dir/depend.make
+++ workspace/build/core/CMakeFiles/node.dir/depend.make
@@ -295,7 +295,12 @@ CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/deta
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/empty__struct.hpp
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/empty__traits.hpp
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/empty__type_support.hpp
+CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/float32__builder.hpp
+CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/float32__struct.hpp
+CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/float32__traits.hpp
+CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/detail/float32__type_support.hpp
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/empty.hpp
+CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/float32.hpp
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/std_msgs/msg/rosidl_generator_cpp__visibility_control.hpp
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/tracetools/config.h
 CMakeFiles/node.dir/src/offb_node.cpp.o: /opt/ros/foxy/include/tracetools/tracetools.h
--- workspace/src/core/src/offb_node.cpp
+++ workspace/src/core/src/offb_node.cpp
@@ -1,6 +1,7 @@
 // ROS libraries and dependencies
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/empty.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 // PX4 libraries and dependencies
 #include "px4_msgs/srv/vehicle_command.hpp"
@@ -9,7 +10,6 @@
 #include "px4_msgs/msg/vehicle_global_position.hpp"
 
 std::array<float,3> global_pos;
-uint16_t command_code;
 
 // Subnormal
 
@@ -28,20 +28,27 @@ public:
         // Definition of subscribers
         global_position_check = this -> create_subscription<px4_msgs::msg::VehicleGlobalPosition>("/fmu/out/vehicle_global_position",qos,
                                 std::bind(&ControlNode::global_pos_cb, this, std::placeholders::_1));
-        service_commander = this -> create_subscription<std_msgs::msg::Empty>("/ddscore/takeoff",qos, 
-                            std::bind(&ControlNode::publish_srv_command, this, std::placeholders::_1));
+        takeoff_srv = this -> create_subscription<std_msgs::msg::Float32>("/ddscore/takeoff",qos, 
+                      std::bind(&ControlNode::set_takeoff, this, std::placeholders::_1));
+        land_srv = this -> create_subscription<std_msgs::msg::Empty>("/ddscore/land",qos,
+                   std::bind(&ControlNode::set_land, this, std::placeholders::_1));
     }
 
 private:
 
     void global_pos_cb(px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg);
+
     void cb_srv_command(const rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future);
-    void publish_srv_command(const std_msgs::msg::Empty::SharedPtr msg);
+    void publish_srv_command(uint16_t command, float alt = 0.0);
+
+    void set_takeoff(const std_msgs::msg::Float32::SharedPtr msg);
+    void set_land(const std_msgs::msg::Empty::SharedPtr msg);
 
     rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr command_cl;
 
     rclcpp::Subscription<px4_msgs::msg::VehicleGlobalPosition>::SharedPtr global_position_check;
-    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr service_commander;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr takeoff_srv;
+    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr land_srv;
 
 };
 
@@ -53,33 +60,36 @@ void ControlNode::cb_srv_command(const rclcpp::Client<px4_msgs::srv::VehicleComm
     auto status = future.wait_for(std::chrono::seconds(1));
     if (status == std::future_status::ready) {
         RCLCPP_INFO(this -> get_logger(), 
-        std::to_string(future.get() -> reply.command));
+        "\n\nCommand sent: "+std::to_string(future.get() -> reply.command)+"\nResult: "+std::to_string(future.get() -> reply.result)
+        +"\nTarget System: "+std::to_string(future.get() -> reply.target_system)+"\nTarget Component: "+std::to_string(future.get() -> reply.target_component)
+        +"\nExternal source: "+std::to_string(future.get() -> reply.from_external)+"\n");
     } else {
         RCLCPP_INFO(this -> get_logger(), "Service in progress");
     }
 }
 
-void ControlNode::publish_srv_command(const std_msgs::msg::Empty::SharedPtr msg) {
-    (void) msg; //Shuts the compiler up about the empty message not being used
-    RCLCPP_INFO(this -> get_logger(), "Hey! I'm going to take off!");
-
+void ControlNode::publish_srv_command(uint16_t command, float alt) {
+    
     auto request = std::make_shared<px4_msgs::srv::VehicleCommand::Request>();
 
-    px4_msgs::msg::VehicleCommand command{};
+    px4_msgs::msg::VehicleCommand msg{};
 
-    command.param5 = global_pos[1]; // Latitude
-    command.param6 = global_pos[2]; // Longitude
-	command.param7 = global_pos[3] + 2.50; // Absolute altitude + takeoff altitude
+    msg.param5 = global_pos[1]; // Latitude
+    msg.param6 = global_pos[2]; // Longitude
 
-	command.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_TAKEOFF;
-	command.target_system = 1;
-	command.target_component = 1;
-	command.source_system = 1;
-	command.source_component = 1;
-	command.from_external = true;
-    command.timestamp = this -> get_clock() -> now().nanoseconds() / 1000;
+    // Absolute altitude + takeoff altitude
+    if (command == 22) msg.param7 = global_pos[3] + alt;  //22 is the takeoff command
+	else msg.param7 = global_pos[3]; 
 
-    request -> request = command;
+	msg.command = command;
+	msg.target_system = 1;
+	msg.target_component = 1;
+	msg.source_system = 1;
+	msg.source_component = 1;
+	msg.from_external = true;
+    msg.timestamp = this -> get_clock() -> now().nanoseconds() / 1000;
+
+    request -> request = msg;
     while(!command_cl -> wait_for_service(std::chrono::seconds(1)))
     {
         if (!rclcpp::ok()) {
@@ -91,10 +101,16 @@ void ControlNode::publish_srv_command(const std_msgs::msg::Empty::SharedPtr msg)
     auto result = command_cl -> async_send_request(request, std::bind(&ControlNode::cb_srv_command, this, std::placeholders::_1));
 }
 
-// void ControlNode::set_takeoff(const std_msgs::msg::Empty::SharedPtr msg) {
-//     (void) msg; //Shuts the compiler up about the empty message not being used
+void ControlNode::set_takeoff(const std_msgs::msg::Float32::SharedPtr msg) {
+    RCLCPP_INFO(this -> get_logger(), "Hey! I'm going to take off!");
+    publish_srv_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_TAKEOFF, msg -> data);
+}
 
-// }
+void ControlNode::set_land(const std_msgs::msg::Empty::SharedPtr msg) {
+    (void) msg; //Shuts the compiler up about the empty message not being used
+    RCLCPP_INFO(this -> get_logger(), "Hey! I'm going to land!");
+    publish_srv_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_LAND);
+}
 
 int main(int argc, char **argv){
 
