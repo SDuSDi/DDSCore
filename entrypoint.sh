8404c4d96aff2d7102411b4566e381d18a460db5
SDuSDi <bmartinez.ext@catec.aero>
2024-05-27 11:56:20 +0200

    Push 27/05/24
    Added comment to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e 
 source microros_ws/install/setup.bash && ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 &
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/microros_ws/install/px4_msgs/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/microros_ws/install/px4_msgs/lib/ #Arregla un problema de path de ddscore
 source /opt/ros/humble/setup.bash && cd workspace && colcon build && source install/setup.bash && ros2 run core node
