e734991b576fff9d1d24dc6a330c525d1b37ec21
SDuSDi <bmartinez.ext@catec.aero>
2024-05-30 13:07:34 +0200

    Push 30/05/24
    Added configuration of enviroment through configMap files
    Linked configuration files to deployment.yaml
    Commented WIP on GUI access from K8s
    Added a blank line on PX4 Dockerfile for aesthetic reasons
    Added wait of 20 seconds on DDSCore start
    Added HEADLESS flag on PX4 SITL start to prevent GUI use

--- entrypoint-px4.sh
+++ entrypoint-px4.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 set -e
-cd PX4-Autopilot && PX4_GZ_STANDALONE=1 make px4_sitl gz_x500
+source microros_ws/install/setup.bash && ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 &
+cd PX4-Autopilot && HEADLESS=1 make px4_sitl gz_x500
