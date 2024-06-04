7b8700153d78af7bc7f962baad58ff6d1a42abc5
SDuSDi <bmartinez.ext@catec.aero>
2024-06-04 14:41:59 +0200

    Push 4/06/24
    Added scripts folder to repository
    Added Kubernetes configmaps and deployment builder to scripts folder for easy use
    Changed all instances of env  to
    Deleted ports from deployment files and added hostNetwork: true
    Deleted DDS agent from PX4 that was there due to a misundestanding
    Modified PX4 Dockerfile to change from parent Dockerfile ros:humble to Ubuntu:20.04

--- entrypoint-px4.sh
+++ entrypoint-px4.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 set -e
-source microros_ws/install/setup.bash && ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 &
 cd PX4-Autopilot && HEADLESS=1 make px4_sitl gz_x500
---
+++ scripts/builder.py
@@ -0,0 +1,33 @@
+import os
+
+if __name__ == "__main__":
+
+    user = os.getlogin()
+    if os.path.exists("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_config.yaml".format(user)):
+        os.remove("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_config.yaml".format(user))
+    if os.path.exists("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_deploy.yaml".format(user)):
+        os.remove("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_deploy.yaml".format(user))
+
+    replicas = int(input("Dame el número de réplicas que necesitas: "))
+
+    config = open("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_config.yaml".format(user),"a")
+    deploy = open("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_deploy.yaml".format(user),"a")
+
+    for i in range(0,replicas):
+        config.writelines(["---\n","\n","apiVersion: v1\n","kind: ConfigMap\n","metadata:\n","  name: fastdds-config-domain-{0}\n".format(i),"  namespace: default\n",
+                           "data:\n","  ROS_DOMAIN_ID: '{0}'\n".format(i),"  RMW_IMPLEMENTATION: rmw_fastrtps_cpp\n","  PX4_UXRCE_DDS_PORT: '{0}'\n".format(8000+i),"\n"])
+
+        deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: ddscore-{0}\n".format(i),"  labels:\n",
+                           "    app: dds_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: dds_l\n","  template:\n","    metadata:\n",
+                           "      labels:\n","        app: dds_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n",
+                           "      - name: ddscore-pod-{0}\n".format(i),"        image: local/ddscore:v1\n","        envFrom:\n","          - configMapRef:\n",
+                           "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
+        
+        deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: px4-{0}\n".format(i),"  labels:\n",
+                           "    app: px4_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: px4_l\n","  template:\n","    metadata:\n",
+                           "      labels:\n","        app: px4_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n","      - name: px4-pod-{0}\n".format(i),
+                           "        image: local/px4:v1\n","        envFrom:\n","          - configMapRef:\n",
+                           "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
+
+    config.close()
+    deploy.close()
