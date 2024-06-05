754925873ac6c59d82b15d600839a8522607be3c
SDuSDi <bmartinez.ext@catec.aero>
2024-06-05 09:52:35 +0200

    Push 5/06/24
    Added PahoMqttCpp and Nlohmannn-Json to DDSCore Dockerfile, needed for Link Node
    Removed sleep from DDSCore entrypoint as it is no longer necessary

--- entrypoint-ddscore.sh
+++ entrypoint-ddscore.sh
@@ -2,4 +2,4 @@
 set -e 
 source microros_ws/install/setup.bash && ros2 run micro_ros_agent micro_ros_agent udp4 --port $PX4_UXRCE_DDS_PORT &
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/microros_ws/install/px4_msgs/lib/ #Arregla un problema de path de ddscore
-source /opt/ros/humble/setup.bash && cd workspace && colcon build && source install/setup.bash && sleep 20 && ros2 run core node
+source /opt/ros/humble/setup.bash && cd workspace && colcon build && source install/setup.bash && ros2 run core node
