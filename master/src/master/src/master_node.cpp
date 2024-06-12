d610b96d169cc02f550db8a92a34d9f2f57abbd7
SDuSDi <bmartinez.ext@catec.aero>
2024-06-12 08:26:23 +0200

    Push 12/06/24
    Added builder files to .gitignore due to them being considered useless for the repository
    Added listener file to master pkg to listen to the status of the drone
    Modified bot publisher and listener to work with namespaces
    Added those namespaces to master main code
    Added listener timer to master main code

--- master/src/master/src/master_node.cpp
+++ master/src/master/src/master_node.cpp
@@ -7,6 +7,7 @@ using json = nlohmann::json; // for convenience
 
 // MQTT files and setup
 #include "publisher.cpp"
+#include "listener.cpp"
 mqtt::async_client *client = NULL;
 
 json tmp_msg = {
@@ -24,18 +25,24 @@ class ROSMaster : public rclcpp::Node
 public:
     ROSMaster(int argc, char **argv) : Node("rosmaster")
     {
-        string	address  = (argc > 1) ? string(argv[1]) : DFLT_SERVER_ADDRESS,
-		        clientID = (argc > 2) ? string(argv[2]) : CLIENT_ID;
+		auto timer_callback = [this]() -> void {
+			json instant_data = listener::data;
+		};
+		timer = this -> create_wall_timer(std::chrono::milliseconds(100), timer_callback);
+		std::cout << "Strange shit done" << std::endl;
+
+        std::string	address  = (argc > 1) ? std::string(argv[1]) : publisher::DFLT_SERVER_ADDRESS,
+		        clientID = (argc > 2) ? std::string(argv[2]) : publisher::CLIENT_ID;
         
         std::cout << "\nInitializing for server '" << address << "'..." << std::endl;
-	    mqtt::async_client client(address, clientID, PERSIST_DIR);
+	    mqtt::async_client client(address, clientID, publisher::PERSIST_DIR);
 
-    	callback cb;
+    	publisher::callback cb;
 	    client.set_callback(cb);
 
         auto connOpts = mqtt::connect_options_builder()
             .clean_session()
-            .will(mqtt::message(TOPIC, LWT_PAYLOAD, strlen(LWT_PAYLOAD), QOS, false))
+            .will(mqtt::message(publisher::TOPIC, publisher::LWT_PAYLOAD, strlen(publisher::LWT_PAYLOAD), publisher::QOS, false))
             .finalize();
 
         std::cout << "Everything OK. Starting..." << std::endl;
@@ -51,13 +58,13 @@ public:
 		std::cout << "Ready to send message. Press any key to continue." << std::endl; std::cin >> x;
 
 		std::cout << "\nSending takeoff message...";
-		action_listener listener;
+		publisher::action_listener listener;
 		tmp_msg["id"] = "0";
 		tmp_msg["command"] = "takeoff";
 		tmp_msg["param1"] = 2.0;
 		std::string PAYLOAD = to_string(tmp_msg);
 		std::cout << " Message sent: " << PAYLOAD << std::endl;
-		mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, PAYLOAD);
+		mqtt::message_ptr pubmsg = mqtt::make_message(publisher::TOPIC, PAYLOAD);
 		mqtt::delivery_token_ptr pubtok = client.publish(pubmsg, nullptr, listener);
 		pubtok->wait();
 
@@ -73,7 +80,7 @@ public:
 		tmp_msg["param5"] = 2.0;
 		PAYLOAD = to_string(tmp_msg);
 		std::cout << " Message sent: " << PAYLOAD << std::endl;
-		pubmsg = mqtt::make_message(TOPIC, PAYLOAD);
+		pubmsg = mqtt::make_message(publisher::TOPIC, PAYLOAD);
 		pubtok = client.publish(pubmsg, nullptr, listener);
 		pubtok->wait();
 
@@ -91,6 +98,7 @@ public:
 
 private:
 
+	rclcpp::TimerBase::SharedPtr timer;
 
 };
 
--- master/src/master/src/publisher.cpp
+++ master/src/master/src/publisher.cpp
@@ -7,84 +7,77 @@
 #include <cstring>
 #include "mqtt/async_client.h"
 
-using namespace std;
+namespace publisher{
 
-const string DFLT_SERVER_ADDRESS	{"mqtt://localhost:1883" };
-const string CLIENT_ID				{"Master MQTT Publisher" };
-const string TOPIC                  {"MQTT-Controller"};
+	const std::string DFLT_SERVER_ADDRESS	{"mqtt://localhost:1883" };
+	const std::string CLIENT_ID				{"Master MQTT Publisher" };
+	const std::string TOPIC                 {"MQTT-Controller"};
+	const std::string PERSIST_DIR {"./persist"};
+	const char* LWT_PAYLOAD = "Last will and testament.";
+	const int  QOS = 1;
+	const auto TIMEOUT = std::chrono::seconds(10);
 
-const string PERSIST_DIR {"./persist"};
+	/////////////////////////////////////////////////////////////////////////////
 
-// const char* PAYLOAD1 = "Hello World!";
-// const char* PAYLOAD2 = "Hi there!";
-// const char* PAYLOAD3 = "Is anyone listening?";
-// const char* PAYLOAD4 = "Someone is always listening.";
+	/**
+	 * A callback class for use with the main MQTT client.
+	 */
+	class callback : public virtual mqtt::callback
+	{
+	public:
+		void connection_lost(const std::string& cause) override {
+			std::cout << "\nConnection lost" << std::endl;
+			if (!cause.empty())
+				std::cout << "\tcause: " << cause << std::endl;
+		}
 
-const char* LWT_PAYLOAD = "Last will and testament.";
+		void delivery_complete(mqtt::delivery_token_ptr tok) override {
+			std::cout << "\tDelivery complete for token: "
+				<< (tok ? tok->get_message_id() : -1) << std::endl;
+		}
+	};
 
-const int  QOS = 1;
+	/////////////////////////////////////////////////////////////////////////////
 
-const auto TIMEOUT = std::chrono::seconds(10);
+	/**
+	 * A base action listener.
+	 */
+	class action_listener : public virtual mqtt::iaction_listener
+	{
+	protected:
+		void on_failure(const mqtt::token& tok) override {
+			std::cout << "\tListener failure for token: "
+				<< tok.get_message_id() << std::endl;
+		}
 
-/////////////////////////////////////////////////////////////////////////////
+		void on_success(const mqtt::token& tok) override {
+			std::cout << "\tListener success for token: "
+				<< tok.get_message_id() << std::endl;
+		}
+	};
 
-/**
- * A callback class for use with the main MQTT client.
- */
-class callback : public virtual mqtt::callback
-{
-public:
-	void connection_lost(const string& cause) override {
-		cout << "\nConnection lost" << endl;
-		if (!cause.empty())
-			cout << "\tcause: " << cause << endl;
-	}
+	/////////////////////////////////////////////////////////////////////////////
 
-	void delivery_complete(mqtt::delivery_token_ptr tok) override {
-		cout << "\tDelivery complete for token: "
-			<< (tok ? tok->get_message_id() : -1) << endl;
-	}
-};
+	/**
+	 * A derived action listener for publish events.
+	 */
+	class delivery_action_listener : public action_listener
+	{
+		std::atomic<bool> done_;
 
-/////////////////////////////////////////////////////////////////////////////
+		void on_failure(const mqtt::token& tok) override {
+			action_listener::on_failure(tok);
+			done_ = true;
+		}
 
-/**
- * A base action listener.
- */
-class action_listener : public virtual mqtt::iaction_listener
-{
-protected:
-	void on_failure(const mqtt::token& tok) override {
-		cout << "\tListener failure for token: "
-			<< tok.get_message_id() << endl;
-	}
+		void on_success(const mqtt::token& tok) override {
+			action_listener::on_success(tok);
+			done_ = true;
+		}
 
-	void on_success(const mqtt::token& tok) override {
-		cout << "\tListener success for token: "
-			<< tok.get_message_id() << endl;
-	}
-};
-
-/////////////////////////////////////////////////////////////////////////////
-
-/**
- * A derived action listener for publish events.
- */
-class delivery_action_listener : public action_listener
-{
-	atomic<bool> done_;
-
-	void on_failure(const mqtt::token& tok) override {
-		action_listener::on_failure(tok);
-		done_ = true;
-	}
-
-	void on_success(const mqtt::token& tok) override {
-		action_listener::on_success(tok);
-		done_ = true;
-	}
-
-public:
-	delivery_action_listener() : done_(false) {}
-	bool is_done() const { return done_; }
-};
+	public:
+		delivery_action_listener() : done_(false) {}
+		bool is_done() const { return done_; }
+	};
+	
+}
