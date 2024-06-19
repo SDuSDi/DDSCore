7c5ff3562a4dd3649a89ebd4043cb4c7ba4716d7
SDuSDi <bmartinez.ext@catec.aero>
2024-06-19 09:30:22 +0200

    Push 19/06/24
    Updated Paho MQTT installation instruxtions on DDSCore and ROSMaster Dockerfiles
    Added minor utilities to ROSMaster Dockerfile for development purposes
    Added instructions to PX4 Dockerfile to modify certain PX4 parameter that was causing trouble
    Rebuilt files from builder.py script
    Link pkg has been silenced upon sending a msg through MQTT

--- workspace/src/link/src/mqtt.cpp
+++ workspace/src/link/src/mqtt.cpp
@@ -44,13 +44,13 @@ namespace mqttlink{
 		}
 
 		void on_success(const mqtt::token& tok) override {
-			std::cout << name_ << " success";
-			if (tok.get_message_id() != 0)
-				std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
-			auto top = tok.get_topics();
-			if (top && !top->empty())
-				std::cout << "\ttoken topic: '" << (*top)[0] << "', ..." << std::endl;
-			std::cout << std::endl;
+			// std::cout << name_ << " success";
+			// if (tok.get_message_id() != 0)
+			// 	std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
+			// auto top = tok.get_topics();
+			// if (top && !top->empty())
+			// 	std::cout << "\ttoken topic: '" << (*top)[0] << "', ..." << std::endl;
+			// std::cout << std::endl;
 		}
 
 	public:
