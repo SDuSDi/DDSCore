eb79d1fd347672f7768ceff63e8f032f25670e4b
SDuSDi <bmartinez.ext@catec.aero>
2024-06-27 09:16:08 +0200

    Push 27/06/24
    Commented if statement to delete dds client from PX4 docker
    Added comment to new instruction in PX4 Dockerfile

--- dockerfiles/entrypoints/entrypoint-px4.sh
+++ dockerfiles/entrypoints/entrypoint-px4.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 set -e
+
+# if [ $PX4_UXRCE_DDS_PORT -eq 8001 ] 
+# then 
+#     cd PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix
+#     sed -i "/uxrce_dds_client start/d" rcS
+#     cd
+# fi
+
 cd PX4-Autopilot && HEADLESS=1 make px4_sitl gz_x500
