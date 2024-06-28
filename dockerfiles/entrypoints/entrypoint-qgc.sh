bcb9b56ce5ec3d4a6a459cbe02687adc41bc15ef
SDuSDi <bmartinez.ext@catec.aero>
2024-06-28 13:56:13 +0200

    Push 28/06/24
    Removed QGC Dockerfile and entrypoint due to finding no use to them
    Adapted builder script to fit new architecture of deployment

--- dockerfiles/entrypoints/entrypoint-qgc.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-set -e
-export DISPLAY=:1.0 && xterm
-#./QGroundControl.AppImage
--- scripts/builder.py
+++ scripts/builder.py
@@ -13,26 +13,33 @@ if __name__ == "__main__":
     config = open("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_config.yaml".format(user),"a")
     deploy = open("/home/{0}/ros2_ws/ddscore/scripts/k8s-builder-files/new_deploy.yaml".format(user),"a")
 
-    deploy.writelines(["apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: mosquitto\n","spec:\n","  replicas: 1\n","  selector:\n",
-                       "    matchLabels:\n","      name: mosquitto\n","  template:\n","    metadata:\n","      labels:\n","        name: mosquitto\n","    spec:\n",
-                       "      hostNetwork: true\n","      containers:\n","        - name: mosquitto\n","          image: eclipse-mosquitto:2.0.18\n","\n"])
+    # deploy.writelines(["apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: mosquitto\n","spec:\n","  replicas: 1\n","  selector:\n",
+    #                    "    matchLabels:\n","      name: mosquitto\n","  template:\n","    metadata:\n","      labels:\n","        name: mosquitto\n","    spec:\n",
+    #                    "      hostNetwork: true\n","      containers:\n","        - name: mosquitto\n","          image: eclipse-mosquitto:2.0.18\n","\n"])
 
     for i in range(0,replicas):
 
         config.writelines(["---\n","\n","apiVersion: v1\n","kind: ConfigMap\n","metadata:\n","  name: fastdds-config-domain-{0}\n".format(i),"  namespace: default\n",
                            "data:\n","  ROS_DOMAIN_ID: '{0}'\n".format(i),"  RMW_IMPLEMENTATION: rmw_fastrtps_cpp\n","  PX4_UXRCE_DDS_PORT: '{0}'\n".format(8000+i),"\n"])
 
-        deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: ddscore-{0}\n".format(i),"  labels:\n",
-                           "    app: dds_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: dds_l\n","  template:\n","    metadata:\n",
-                           "      labels:\n","        app: dds_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n",
-                           "      - name: ddscore-pod-{0}\n".format(i),"        image: local/ddscore:v1\n","        envFrom:\n","          - configMapRef:\n",
-                           "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
+        # deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: ddscore-{0}\n".format(i),"  labels:\n",
+        #                    "    app: dds_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: dds_l\n","  template:\n","    metadata:\n",
+        #                    "      labels:\n","        app: dds_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n",
+        #                    "      - name: ddscore-pod-{0}\n".format(i),"        image: local/ddscore:v1\n","        envFrom:\n","          - configMapRef:\n",
+        #                    "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
         
-        deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: px4-{0}\n".format(i),"  labels:\n",
-                           "    app: px4_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: px4_l\n","  template:\n","    metadata:\n",
-                           "      labels:\n","        app: px4_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n","      - name: px4-pod-{0}\n".format(i),
-                           "        image: local/px4:v1\n","        envFrom:\n","          - configMapRef:\n",
-                           "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
+        # deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: px4-{0}\n".format(i),"  labels:\n",
+        #                    "    app: px4_l\n","spec:\n","  replicas: 1\n","  selector:\n","    matchLabels:\n","      app: px4_l\n","  template:\n","    metadata:\n",
+        #                    "      labels:\n","        app: px4_l\n","    spec:\n","      hostNetwork: true\n","      containers:\n","      - name: px4-pod-{0}\n".format(i),
+        #                    "        image: local/px4:v1\n","        envFrom:\n","          - configMapRef:\n",
+        #                    "              name: fastdds-config-domain-{0}\n".format(i),"\n"])
+
+        deploy.writelines(["---\n","\n","apiVersion: apps/v1\n","kind: Deployment\n","metadata:\n","  name: drone-{0}\n".format(i),"spec:\n","  replicas: 1\n",
+                           "  selector:\n","    matchLabels:\n","      app: drn-{0}\n".format(i),"  template:\n","    metadata:\n","      labels:\n",
+                           "        app: drn-{0}\n".format(i),"    spec:\n","      containers:\n","      - name: ddscore\n","        image: local/ddscore:v1\n",
+                           "        ports:\n","        - containerPort: 1883\n","        envFrom:\n","          - configMapRef:\n",
+                           "              name: fastdds-config-domain-{0}\n".format(i),"      - name: px4\n","        image: local/px4:v1\n","        envFrom:\n",
+                           "          - configMapRef:\n","              name: fastdds-config-domain-{0}\n".format(i),"\n"])
 
     config.close()
     deploy.close()
